d5feeec4ea330f767336df78fe39728d33a7a1e1
RutSzymon <rut.szymon@gmail.com>
2020-06-02 16:23:52 +0200

    Added setControls function

--- javascripts/paint/paint.js
+++ javascripts/paint/paint.js
@@ -4,6 +4,7 @@ class Paint {
     this.img.addEventListener("load", () => { //funkcja strzałkowa by nie zgubić this
       this.canvasCnt = document.querySelector(".paint-canvas-cnt");
       this.createCanvas();
+      this.setControls();
     });
     this.img.src = "canvas-bg.png";
   }
@@ -17,4 +18,16 @@ class Paint {
     this.canvasCnt.appendChild(this.canvas);
     this.ctx = this.canvas.getContext("2d");
   }
+
+  setControls() {
+      //element pobierania wielkości pędzla
+      this.sizeElem = document.querySelector(".paint-size");
+
+      //element pokazujący wielkość pędzla
+      this.sizeElemVal = document.querySelector(".paint-size-val");
+      this.sizeElemVal.innerText = this.sizeElem.value;
+
+      //element do pobierania koloru
+      this.colorElem = document.querySelector(".paint-color");
+    }
 }
