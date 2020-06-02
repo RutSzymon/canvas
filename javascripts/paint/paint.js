89a2dc3a114e374b4539f304a40cccd0854de513
RutSzymon <rut.szymon@gmail.com>
2020-06-02 16:22:59 +0200

    Canvas creation

--- javascripts/paint/paint.js
+++ javascripts/paint/paint.js
@@ -7,4 +7,14 @@ class Paint {
     });
     this.img.src = "canvas-bg.png";
   }
+
+  createCanvas() {
+    const canvas = document.createElement("canvas");
+    canvas.width = this.canvasCnt.offsetWidth;
+    canvas.height = this.canvasCnt.offsetHeight;
+
+    this.canvas = this.createCanvas();
+    this.canvasCnt.appendChild(this.canvas);
+    this.ctx = this.canvas.getContext("2d");
+  }
 }
