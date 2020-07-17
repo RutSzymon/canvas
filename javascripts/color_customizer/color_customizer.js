88798778eb2ed6964e46e8313d138efb7b75563c
RutSzymon <rut.szymon@gmail.com>
2020-07-17 11:01:53 +0200

    Added color_customizer scaffold

---
+++ javascripts/color_customizer/color_customizer.js
@@ -0,0 +1,4 @@
+const scene = new THREE.Scene();
+const canvas = document.querySelector("#c");
+const renderer = new THREE.WebGLRenderer({ canvas, antialias: true });
+document.body.appendChild(renderer.domElement);
