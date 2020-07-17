862de14d2d8e7ac30fe3d124f7a9036f626829c0
RutSzymon <rut.szymon@gmail.com>
2020-07-17 11:11:00 +0200

    Added animate function

--- javascripts/color_customizer/color_customizer.js
+++ javascripts/color_customizer/color_customizer.js
@@ -1,4 +1,22 @@
 const scene = new THREE.Scene();
 const canvas = document.querySelector("#c");
 const renderer = new THREE.WebGLRenderer({ canvas, antialias: true });
+
 document.body.appendChild(renderer.domElement);
+
+var cameraFar = 5;
+var camera = new THREE.PerspectiveCamera(
+  50,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+camera.position.z = cameraFar;
+camera.position.x = 0;
+
+function animate() {
+  renderer.render(scene, camera);
+  requestAnimationFrame(animate);
+}
+
+animate();
