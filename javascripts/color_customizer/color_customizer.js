5a7f5e6536cfff024b7df09344b249156bc04531
RutSzymon <rut.szymon@gmail.com>
2020-07-17 18:34:25 +0200

    Added local textures

--- javascripts/color_customizer/color_customizer.js
+++ javascripts/color_customizer/color_customizer.js
@@ -7,28 +7,27 @@ var activeOption = "legs";
 const TRAY = document.getElementById("js-tray-slide");
 const colors = [
   {
-    texture: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/1376484/wood_.jpg",
+    texture: "images/color_customizer/wood_.jpg",
     size: [2, 2, 2],
     shininess: 60,
   },
   {
-    texture: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/1376484/fabric_.jpg",
+    texture: "images/color_customizer/fabric_.jpg",
     size: [4, 4, 4],
     shininess: 0,
   },
   {
-    texture:
-      "https://s3-us-west-2.amazonaws.com/s.cdpn.io/1376484/pattern_.jpg",
+    texture: "images/color_customizer/pattern_.jpg",
     size: [8, 8, 8],
     shininess: 10,
   },
   {
-    texture: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/1376484/denim_.jpg",
+    texture: "images/color_customizer/denim_.jpg",
     size: [3, 3, 3],
     shininess: 0,
   },
   {
-    texture: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/1376484/quilt_.jpg",
+    texture: "images/color_customizer/quilt_.jpg",
     size: [6, 6, 6],
     shininess: 0,
   },
